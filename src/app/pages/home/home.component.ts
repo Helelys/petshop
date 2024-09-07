9ab9d9519dacd94533a9052eaff1388441bccc93
helelys <tallyslabanca20@gmail.com>
2024-09-07 10:35:36 -0300

    hero banner feito

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactHeaderComponent } from "../../components/contact-header/contact-header.component";
 import { HeaderBarComponent } from "../../components/header-bar/header-bar.component";
+import { HeroBannerComponent } from "../../components/hero-banner/hero-banner.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ContactHeaderComponent, HeaderBarComponent],
+  imports: [ContactHeaderComponent, HeaderBarComponent, HeroBannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
